023e81392dec605f6eff9113613bb18a27b6a22e
shibilabasheer <shibila.abasheer@gmail.com>
2025-06-06 17:53:14 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Product from './components/Product';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import "./css/product.css";
+import "./css/Product.css";
 import Header from './components/header';
 
 function App() {
